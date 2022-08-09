c28638e9d6a5c436594df3b313cd9403d106ad93
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-09 20:43:07 +0300

    updated the code

--- task_26/Program.cs
+++ task_26/Program.cs
@@ -2,12 +2,12 @@
 на вход число и выдаёт количество цифр в числе.
 456 -> 3
 78 -> 2
-89126 -> 5*/
+89126 -> 5
+*/
 
+Console.Clear();
 Console.Write("Введите число: ");
 string? data = Console.ReadLine();
-
-
 Console.WriteLine($"{data} ->  {GetDigitCount(Convert.ToInt32(data))}");
 
 
@@ -23,4 +23,27 @@ int GetDigitCount(int num)
     return result;
 }
 
+Console.WriteLine();
+
+//второй вариант
+
+//Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int GetNum (int number)
+{
+    int index = 1;
+    int x = 0;
+    while (index <= a)
+    {
+        x = x+1;
+        index = index*10;
+    }
+    return x;
+}
+
+int res = GetNum(a);
+Console.WriteLine($"Количество цифр в числе =  {res}");
+Console.WriteLine();
 
