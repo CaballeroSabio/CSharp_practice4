75e663c234601a588a3b727340ed5073e3c63011
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-09 19:20:02 +0300

    created the new code

---
+++ task_35/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 35: Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в
+отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5 */
+
+Console.Clear();
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+int GetAmount (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        sum++;
+    }
+    return sum;
+}
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == 0) Console.Write("[");
+        if(i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+int[] array = CreateArrayRndInt(123, 1, 124);
+PrintArray(array);
+int result = GetAmount(array);
+Console.WriteLine();
+Console.WriteLine(result);
+
+
