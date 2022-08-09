9e76ec58ed642eb3047de76249419e7cc8a0319f
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-09 19:19:10 +0300

    created the new code

---
+++ task_26/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+Console.Write("Введите число: ");
+string? data = Console.ReadLine();
+
+
+Console.WriteLine($"{data} ->  {GetDigitCount(Convert.ToInt32(data))}");
+
+
+int GetDigitCount(int num)
+{
+    int result = 0;
+    while(num >= 1)
+    {
+        num = num/10;
+        result++; 
+    }
+    
+    return result;
+}
+
+
