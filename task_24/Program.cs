a0d8f7102d838f310d69792caa6cf2d425d6ce2f
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-09 20:42:52 +0300

    updated the code

--- task_24/Program.cs
+++ task_24/Program.cs
@@ -6,13 +6,26 @@
 8 -> 36
 */
 
+Console.Clear();
 Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.WriteLine());
+int num = Convert.ToInt32(Console.ReadLine());
 
-int sum = 0;
-for (int i = 1; i <= num; i++)
-{ 
-    sum = sum + i;
+int GetSum (int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    { 
+        sum = sum + i;
+    }
+    return sum;
 }
 
-Console.WriteLine()
+void Print (int n, int res)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {n} = {res}");
+}
+
+int result = GetSum(num);
+Print (num, result);
+
+Console.WriteLine();
