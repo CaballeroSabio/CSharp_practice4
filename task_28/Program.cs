2b2c10a13c9d2249aa47eb57313d27bf83440f8d
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-10 12:59:21 +0300

    created the new code

---
+++ task_28/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 28: Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int GetDigit (int number)
+{
+    int count = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        count = count*i;
+    }
+    return count;
+}
+
+int res = GetDigit (n);
+Console.WriteLine($"{n} -> {res}");
