7984ba4b1ec5c9effbada6d2154a7c8cc03cc6a7
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-10 20:42:22 +0300

    updated the code

--- task_30/Program.cs
+++ task_30/Program.cs
@@ -5,4 +5,46 @@
 
 Console.Clear();
 
-int[] array = new int[8];
+int[] array = new int[8]; // or int[] array = {5, 9, 3, 5, 4, 7, 7, 1};
+int rndNum = new Random().Next(0, 2);
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 0) Console.Write("[");
+    Console.Write(array[i] + ", ");    
+}
+Console.Write("]");
+Console.WriteLine();
+
+// Вариант второй
+
+int[] array2 = GetRandomArray(12);
+WriteArrayToConsole(array);
+
+int[] GetRandomArray(int arrayLength)
+{
+    Random rnd = new Random();
+    int[] arr = new int[arrayLength];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+    return arr;
+}
+
+void WriteArrayToConsole(int[] array2)
+{
+    //Console.Clear();
+    Console.Write("[");
+    for (int i= 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length -1)
+            Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
