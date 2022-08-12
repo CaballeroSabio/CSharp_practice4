267764ddfa0626bb1f33aee8ae0599857f09f832
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-12 16:57:00 +0300

    modified the file

---
+++ task_29/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов, заполненный
+псевдослучайными числами и выводит их на экран.
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+*/
+
+Console.Clear();
+
+int[] GetRandomArray(int arrayLength)
+{
+    Random rnd = new Random();
+    int[] arr = new int[arrayLength];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0,100);
+    }
+    return arr;
+}
+
+void WriteArrayToConsole(int[] array)
+{
+    Console.Clear();
+    Console.Write("[");
+    for (int i= 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length -1)
+            Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = GetRandomArray(8);
+Console.WriteLine(array);
+WriteArrayToConsole(array);
+Console.WriteLine();
