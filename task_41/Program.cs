6f4542000339d46cdb9d29d9545913691034f736
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-08-25 21:52:53 +0300

    created the new code

---
+++ task_41/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 41:
+ * Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+ * 0, 7, 8, -2, -2 -> 2
+ * -1, -7, 567, 89, 223-> 3
+ */
+
+Console.Clear();
+// предложить пользователю ввести пять чисел
+Console.Write("Ведите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите четвертое число: ");
+int num4 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите пятое число: ");
+int num5 = Convert.ToInt32(Console.ReadLine());
+// записать числа в переменную 
+
+// подсчитать количество положительных чисел
+int CountPosNum(int a, int b, int c, int d, int e)
+{
+    int count = 0;
+    // при условии, что число больше 0, отправлять в 
+    if (a > 0) a = 1; else a = 0;
+    if (b > 0) b = 1; else b = 0;
+    if (c > 0) c = 1; else c = 0;
+    if (d > 0) d = 1; else d = 0;
+    if (e > 0) e = 1; else e = 0;
+    count = a + b + c + d + e;
+    return count; // CountPosNum(a,b,c,d,e);
+}
+// вывести результат
+int result = CountPosNum(num1, num2, num3, num4, num5);
+Console.Write($"Количество чисел больших нуля равно -> {result}");
